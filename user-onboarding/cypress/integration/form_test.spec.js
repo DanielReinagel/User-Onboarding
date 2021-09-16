bfd13191147156c99a35b365b2da86a3a4bfac63
DanielReinagel <daniel.e.reinagel@gmail.com>
2021-09-16 16:26:14 -0700

    added good error testing

--- user-onboarding/cypress/integration/form_test.spec.js
+++ user-onboarding/cypress/integration/form_test.spec.js
@@ -7,6 +7,49 @@ describe('Form App Tests', ()=>{
     const emailInput = () => cy.get('input[name=email]');
     const pwInput = () => cy.get('input[name=password]');
     const toSInput = () => cy.get('input[name=toS]');
+    const submitBtn = () => cy.get('input[type=submit]');
+
+    const noName = 'A name is required';
+    const noEmail = 'An email is required';
+    const noPw = 'A password is required';
+    const toSNotChecked = 'You must agree to the Terms of Service';
+    const shortName = 'The name must include at least 3 characters';
+    const invalidEmail = 'A valid email address is required';
+    const shortPw = 'The Password must include at least 3 characters';
+    const checkErrors = (nameErr, emailErr, pwErr, toSErr) => {
+        if(nameErr){
+            cy.contains(nameErr).should('exist');
+        } else {
+            cy.contains(noName).should('not.exist');
+            cy.contains(shortName).should('not.exist');
+        }
+        if(emailErr){
+            cy.contains(emailErr).should('exist');
+        } else {
+            cy.contains(noEmail).should('not.exist');
+            cy.contains(invalidEmail).should('not.exist');
+        }
+        if(pwErr){
+            cy.contains(pwErr).should('exist');
+        } else {
+            cy.contains(noPw).should('not.exist');
+            cy.contains(shortPw).should('not.exist');
+        }
+        if(toSErr){
+            cy.contains(toSErr).should('exist');
+        } else {
+            cy.contains(toSNotChecked).should('not.exist');
+        }
+        if(nameErr||emailErr||pwErr||toSErr){
+            submitBtn().should('be.disabled');
+        } else {
+            submitBtn().should('not.be.disabled');
+        }
+    }
+
+    const backspaces = (num) => {
+        return '{backspace}'.repeat(num);
+    }
 
     it('test self', ()=>{
         expect(2).to.equal(2);
@@ -39,12 +82,67 @@ describe('Form App Tests', ()=>{
         });
     });
 
-    describe('Form Errors Tests', ()=>{
+    describe('Form Errors and Submit Button Disabled', ()=>{
         it('Starting errors', ()=>{
-            cy.contains('A name is required').should('exist');
-            cy.contains('An email is required').should('exist');
-            cy.contains('A password is required').should('exist');
-            cy.contains('You must agree to the Terms of Service').should('exist');
+            checkErrors(noName, noEmail, noPw, toSNotChecked);
+        });
+        it('Name Errors', ()=>{
+            nameInput().type('Te');
+            checkErrors(shortName, noEmail, noPw, toSNotChecked);
+            nameInput().type('st Name');
+            checkErrors('', noEmail, noPw, toSNotChecked);
+            nameInput().type(backspaces(7));
+            checkErrors(shortName, noEmail, noPw, toSNotChecked);
+            nameInput().type(backspaces(2));
+            checkErrors(noName, noEmail, noPw, toSNotChecked);
+        });
+        it('Email Errors', ()=>{
+            emailInput().type('TestEmail123@email');
+            checkErrors(noName, invalidEmail, noPw, toSNotChecked);
+            emailInput().type('.com');
+            checkErrors(noName, '', noPw, toSNotChecked);
+            emailInput().type(backspaces(4));
+            checkErrors(noName, invalidEmail, noPw, toSNotChecked);
+            emailInput().type(backspaces(18));
+            checkErrors(noName, noEmail, noPw, toSNotChecked);
+        });
+        it('Password Errors', ()=>{
+            pwInput().type('Pa');
+            checkErrors(noName, noEmail, shortPw, toSNotChecked);
+            pwInput().type('ssword123');
+            checkErrors(noName, noEmail, '', toSNotChecked);
+            pwInput().type(backspaces(9));
+            checkErrors(noName, noEmail, shortPw, toSNotChecked);
+            pwInput().type(backspaces(2));
+            checkErrors(noName, noEmail, noPw, toSNotChecked);
+        });
+        it('Terms of Service Errors', ()=>{
+            toSInput().click();
+            checkErrors(noName, noEmail, noPw, '');
+            toSInput().click();
+            checkErrors(noName, noEmail, noPw, toSNotChecked);
+        });
+        it('Full Form Submission Process Errors', ()=>{
+            nameInput().type('Te');
+            checkErrors(shortName, noEmail, noPw, toSNotChecked);
+            nameInput().type('st Name');
+            checkErrors('', noEmail, noPw, toSNotChecked);
+
+            emailInput().type('TestEmail123@email');
+            checkErrors('', invalidEmail, noPw, toSNotChecked);
+            emailInput().type('.com');
+            checkErrors('', '', noPw, toSNotChecked);
+
+            pwInput().type('Pa');
+            checkErrors('', '', shortPw, toSNotChecked);
+            pwInput().type('ssword123');
+            checkErrors('', '', '', toSNotChecked);
+
+            toSInput().click();
+            checkErrors('', '', '', '');
+
+            submitBtn().click();
+            checkErrors(noName, noEmail, noPw, toSNotChecked);
         });
     });
     describe('Form Submission Tests', ()=>{
@@ -53,9 +151,13 @@ describe('Form App Tests', ()=>{
             emailInput().type('TestEmail123@email.com');
             pwInput().type('Password123');
             toSInput().click();
-            cy.get('input[type=submit]').click();
+            submitBtn().click();
             cy.contains('Test Name').should('exist');
             cy.contains('TestEmail123@email.com').should('exist');
+            nameInput().should('have.value', '');
+            emailInput().should('have.value', '');
+            pwInput().should('have.value', '');
+            toSInput().should('not.be.checked');
         });
     });
 });
